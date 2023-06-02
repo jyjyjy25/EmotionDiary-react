a918237652e9a3cfc32dd31bd35fb5d582219a17
jyjyjy25 <jyjyjy7418@gmail.com>
2023-06-02 16:03:31 +0900

    7-3 페이지 라우팅2 - React Router 응용

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,32 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const mode = searchParams.get("mode");
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 수정 페이지입니다.</p>
+      <button onClick={() => setSearchParams({ who: "jykim" })}>
+        QS 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
