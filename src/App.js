f301d649b59b075a0753aa1ac8aa6f5bff9c7292
jyjyjy25 <jyjyjy7418@gmail.com>
2023-06-08 00:43:23 +0900

    7-4 프로젝트 기초공사 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import RouteTest from "./components/RouteTest";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
+// COMPONENTS
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+
 function App() {
+  const env = process.env; // process.env.PUBLIC_URL이 작동하지 않을 경우 작성
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}
+            />
+          }
+        />
         <h2>App.js</h2>
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+
+        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
+
+        {/* <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} /> */}
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
