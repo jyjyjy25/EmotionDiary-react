ffd93e675d4e66755ffc1a1159fabda00e5286ff
jyjyjy25 <jyjyjy7418@gmail.com>
2023-06-08 16:18:54 +0900

    7-6 페이지 구현 - 홈(/)

--- src/App.js
+++ src/App.js
@@ -37,8 +37,47 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: "오늘의일기 1번",
+    date: 1686206344258,
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "오늘의일기 2번",
+    date: 1686206344299,
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "오늘의일기 3번",
+    date: 1686206344300,
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "오늘의일기 4번",
+    date: 1686206344301,
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "오늘의일기 5번",
+    date: 1686206344302,
+  },
+  {
+    id: 6,
+    emotion: 2,
+    content: "오늘의일기 6번",
+    date: 1786206344302,
+  },
+];
+
 function App() {
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, [dummyData]);
 
   const dataId = useRef(0);
   // CREATE
